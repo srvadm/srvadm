a046368c4d8b8e1e0480fc7b79e48d4cb0e21192
tinonaumann <tino@naumix.de>
2020-04-10 22:34:17 +0200

    upade

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
-# create Swarm
-docker swarm init --advertise-addr=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+# fill .env files
 
 # install Traefik Proxy
 docker network create --driver=overlay traefik-public
 docker stack deploy -c <(docker-compose --project-directory /opt/srvadm/system/services/traefik/ -f /opt/srvadm/system/services/traefik/docker-compose.yml config) traefik
 
 # install SrvAdm-System
-mkdir -p /opt/srvadm/system/volumes/certificates/
+sudo mkdir -p /opt/srvadm/system/volumes/certificates/
 
-# fill .env files
 
 #docker stack deploy -c <(docker-compose --project-directory /opt/srvadm/system/services/srvadm/ -f /opt/srvadm/system/services/srvadm/docker-compose.yml config) srvadm
 
