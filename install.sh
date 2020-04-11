59fe0560932164ffe50791f9ab698334c76cd279
tinonaumann <tino@naumix.de>
2020-04-11 21:30:48 +0200

    update

--- install.sh
+++ install.sh
@@ -1,18 +1,15 @@
 # fill .env files
 
+sudo mkdir -p /opt/srvadm/system/volumes/{acme,certificates}/
+
 # install Traefik Proxy
-docker network create --driver=overlay traefik-public
+docker network create --driver=overlay --attachable traefik-public
 docker stack deploy -c <(docker-compose --project-directory /opt/srvadm/system/services/traefik/ -f /opt/srvadm/system/services/traefik/docker-compose.yml config) traefik
 
 # install SrvAdm-System
-sudo mkdir -p /opt/srvadm/system/volumes/certificates/
-
 
 #docker stack deploy -c <(docker-compose --project-directory /opt/srvadm/system/services/srvadm/ -f /opt/srvadm/system/services/srvadm/docker-compose.yml config) srvadm
 
 # install SrvAdm-Mail
 #docker stack deploy -c <(docker-compose --project-directory /opt/srvadm/system/services/mail/ -f /opt/srvadm/system/services/mail/docker-compose.yml config) mail
-
-
-
 
